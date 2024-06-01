bf9ba156ee50334bf1b0d7a4007bb3f269691077
Fugu-Lin <53990453+Fugu-Lin@users.noreply.github.com>
2024-06-01 10:18:12 +0800

    更改grounding_search_servuce

--- service/grounding_search_service.py
+++ service/grounding_search_service.py
@@ -1,5 +1,4 @@
 from vertexai.generative_models import GenerationConfig, GenerativeModel, Tool
-from vertexai.preview.generative_models import grounding
 from bs4 import BeautifulSoup
 import requests
 import os
@@ -12,7 +11,11 @@ class GroundingSearchService:
         
     def fetch_article_content(self, url):
         try:
-            response = requests.get(url)
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+            }
+                
+            response = requests.get(url, headers=headers)
             response.raise_for_status()
             soup = BeautifulSoup(response.text, 'html.parser')
             content = soup.get_text()
@@ -22,17 +25,46 @@ class GroundingSearchService:
         
     def google_search(self, query):
         url = 'https://www.googleapis.com/customsearch/v1'
-        params = {'q': query, 'key': self.api_key, 'cx': self.cse_id}
+        params = {'q': query, 'key': self.api_key, 'cx': self.cse_id, 'num': 3}
         response = requests.get(url, params=params)
         result = response.json()
-        return result
+        
+        web_contents = ''
+        
+        # Parse and extract information
+        cnt = 0
+        if 'items' in result:
+            for item in result['items']:
+                title   = item.get('title')
+                link    = item.get('link')
+                snippet = item.get('snippet')
+                cnt += 1
+                # print(f"Title: {title}")
+                # print(f"Link: {link}")
+                # print(f"Snippet: {snippet}")
+                # print()
+                
+                web_content = self.fetch_article_content(link).strip()
+                
+                web_contents += web_content
+        else:
+            print("No results found.")
+        
+        return web_contents
 
-    def process(self, search_keywords):        
+    def process(self, search_keywords):
+        model = GenerativeModel(model_name="gemini-1.5-pro-001")
         responses = []
         i = 1
+        
         for keyword in search_keywords:
             response = self.google_search(keyword)
-            response = f"The {i} th article: {response}"
+            response = f"The {i} th search result: {response}"
             i += 1
             responses.append(response)
         return ', '.join(response for response in responses)
+
+if __name__ == '__main__':
+    search_keyword = ['AI video marketing tutorial Medium']
+    groundingSearchService = GroundingSearchService()
+    groundingSearchService.process(search_keyword)
